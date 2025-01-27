85bdecf646c244eddbe8fe999d5bb07fb34d3364
rehanadm <arehan.techno@gmail.com>
2025-01-27 05:35:38 +0000

    docker

---
+++ Docker/Docker_on_RHEL.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+###Install Docker Engine on RHEL###
+# Uninstall old versions
+sudo dnf remove docker; docker-client; docker-client-latest; docker-common; docker-latest; docker-latest-logrotate; docker-logrotate; docker-engine; podman; runc
+#Set up the repository
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+# To install the latest version, run
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Start Docker Engine.
+sudo systemctl enable --now docker
